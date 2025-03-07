6d6961e972bad530564a3da7486da2ecbbd089e4
Isabela Mantilla <i.mantilla@uniandes.edu.co>
2025-03-07 13:33:11 -0500

    cambio medico

--- src/main/java/co/edu/uniandes/dse/parcialprueba/entities/MedicoEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/entities/MedicoEntity.java
@@ -23,4 +23,5 @@ public class MedicoEntity extends BaseEntity {
     @PodamExclude
     @ManyToMany(fetch = FetchType.LAZY)
     private List<EspecialidadEntity> especialidades = new ArrayList<>();
+
 }
